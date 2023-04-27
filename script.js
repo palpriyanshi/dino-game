e3fcc2ee31f49ee58044e6d9d11b9b1acc67dd3f
palpriyanshi <pal.priyanshi29@gmail.com>
2023-04-27 15:38:54 +0530

    added scoreboard

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+var score = 0;
+
 function jump(){
     if(dino.classList != "jump") {
         dino.classList.add("jump");
@@ -11,12 +13,17 @@ function jump(){
     }    
 }
 
+
 let isAlive = setInterval(function () {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top")); 
     let cactusLeft = parseInt(window.getComputedStyle(cactus ).getPropertyValue("left"));
 
     if(cactusLeft<40 && cactusLeft > 0 && dinoTop>= 140) {
         alert("Game Over!");
+        score = 0;
+    }
+    else{
+        this.score = document.getElementById("score").innerHTML = ++score;
     }
 }, 10)
 
@@ -24,3 +31,4 @@ let isAlive = setInterval(function () {
 document.addEventListener("keydown", function (event){
     jump();
 })
+
